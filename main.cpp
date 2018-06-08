48c2c5a2299417be99b700111d4e8effd9c5a9cc
LiuShellway <494708681@qq.com>
2018-06-08 16:16:34 +0800

    fix bug

--- main.cpp
+++ main.cpp
@@ -6,30 +6,20 @@ int main()
 {
 	
 	class chess c;
-	std::vector<struct position> r;
-	c.print();
-	r = c.findall();
-	c.put(r[0]);
-	std::cout << (int)r[0].row << " " << (int)r[0].col << std::endl;
-	std::cout << std::endl;
-	c.print();
-	r.clear();
-	r = c.findall();
-	for (unsigned int i = 0; i < r.size(); i++)
-	{
-		std::cout << (int)r[i].row << " " << (int)r[i].col << " " << (int)r[i].num << std::endl;
-	}
-  printf("turn: %d\n", c.get_turn());
 	MCTStree* Tree = new MCTStree(c,c.get_turn());
-  printf("%d\n", Tree->root->get_state().get_turn());
+	Tree->root->state.print();
 	struct position pos;
+	
 	if(Tree->MCTSsearch(15, pos))
 	{
+		c.print();
+		cout << "the turn is " << (int)c.get_turn() << endl;
+		cout << (int)pos.row << ", " << (int)pos.col << endl;
 		c.put(pos);
 	}
 	else
 	{
 		cout << "PASS" << endl;
 	}
-
+	
 }
